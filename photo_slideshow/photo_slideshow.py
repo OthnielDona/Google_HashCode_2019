08f9d7d848ce9199c89029739498e87fd5e6b03b
OthnielDona <othnieldona@gmail.com>
2019-03-13 22:19:57 +0100

    replaced the recursion with loops

--- photo_slideshow/photo_slideshow.py
+++ photo_slideshow/photo_slideshow.py
@@ -1,3 +1,9 @@
+from collections import defaultdict
+from math import *
+import random
+from random import shuffle
+
+
 def read_file(fname):
     # nombre de photo
     num = 0
@@ -32,39 +38,39 @@ def read_file(fname):
     return num, photosVert, photosHor
 
 def compareTags(tag1, tag2):
-    if set(tag1) < set(tag2) or set(tag2) < set(tag1):
+    if len((set(tag1)).intersection(set(tag2))) < 1 :
         return False
     else:
         return True
 
-def matchPhotosVert(photosVert, collect = [], i = 1):
+def matchPhotosVert(photosVert, collect = []):
     if len(photosVert) > 1:
-        if compareTags(photosVert[0]['tags'], photosVert[i]['tags']):
-            collect.append({
-                'ids':str(photosVert[0]['id']) + ' ' + str(photosVert[i]['id']),
-                'tags':list(set(photosVert[0]['tags'] + photosVert[i]['tags']))
-            })
-            photosVert.pop(i); photosVert.pop(0)
-            matchPhotosVert(photosVert, collect, i)
-        else:
-            i += 1
-            matchPhotosVert(photosVert, collect, i)
+        i = 1
+        for photo in photosVert:
+            if compareTags(photosVert[0]['tags'], photosVert[i]['tags']):
+                collect.append({
+                    'ids':str(photosVert[0]['id']) + ' ' + str(photosVert[i]['id']),
+                    'tags':list(set(photosVert[0]['tags'] + photosVert[i]['tags']))
+                })
+                photosVert.pop(i); photosVert.pop(0)
+            else:
+                i += 1
 
     return collect
 
-def createSlide(collPhotos, slide = [], i = 1):
+def createSlide(collPhotos, slide = []):
     if len(collPhotos) > 1:
-        if compareTags(collPhotos[0]['tags'], collPhotos[i]['tags']):
-            slide.append(str(collPhotos[0]['ids']))
-            slide.append(str(collPhotos[i]['ids']))
-            collPhotos.pop(i), collPhotos.pop(0)
-            if len(collPhotos) == 1:
+        i = 1
+        for photo in collPhotos:
+            if compareTags(collPhotos[0]['tags'], collPhotos[i]['tags']):
                 slide.append(str(collPhotos[0]['ids']))
-                return slide
-            createSlide(collPhotos, slide, i)
-        else:
-            i += 1
-            createSlide(collPhotos, slide, i)
+                slide.append(str(collPhotos[i]['ids']))
+                collPhotos.pop(i), collPhotos.pop(0)
+                if len(collPhotos) == 1:
+                    slide.append(str(collPhotos[0]['ids']))
+                    return slide
+            else:
+                i += 1
     elif len(collPhotos) == 1:
         slide.append(str(collPhotos[0]['ids']))
         return slide
@@ -72,7 +78,7 @@ def createSlide(collPhotos, slide = [], i = 1):
     return slide
 
 def write_file(num, coll):
-    with open('slideshow.txt', 'w') as f:
+    with open('e_output.txt', 'w') as f:
         f.write(str(num)+'\n')
         for n in coll:
             f.write(str(n)+'\n')
@@ -81,15 +87,27 @@ def write_file(num, coll):
 
 if __name__ == '__main__':
     # checked
-    num, photosVertical, photosHorizontal = read_file('a_example.txt')
+    num, photosVertical, photosHorizontal = read_file('e_shiny_selfies.txt')
 
     collect_photos_vert = matchPhotosVert(photosVertical)
     collect_photos_hor = []
     for photo in photosHorizontal:
         collect_photos_hor.append({'ids':photo['id'], 'tags':photo['tags']})
 
-    collect_photos = collect_photos_hor + collect_photos_vert
-
-    slideshow = createSlide(collect_photos)
+    collect_photos = collect_photos_vert + collect_photos_hor
     
+    slideshow=[]
+    while len(slideshow) < 19427:
+        perm = list(range(len(collect_photos)))
+        random.shuffle(perm)
+        collect_photos = [collect_photos[index] for index in perm]
+
+        # print(collect_photos)
+        
+        slideshow = createSlide(collect_photos)
+        
+        print(len(slideshow))
+        pass
+    
+
     write_file(len(slideshow), slideshow)
